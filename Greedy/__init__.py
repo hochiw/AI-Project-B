829156eb57940a2e97945e4c4af96779b230de74
TakoYaki <rico5504@gmail.com>
2019-05-12 16:42:43 +1000

    Updated heuristic for minimax

---
+++ Greedy/__init__.py
@@ -0,0 +1,1 @@
+from Greedy.player import Player
--- SquareBox_Minimax/player.py
+++ SquareBox_Minimax/player.py
@@ -4,7 +4,10 @@ from copy import deepcopy
 from random import choice
 import sys
 
-
+# Bitboards that represent the vertical mask
+_VERTICAL = [int(283691315109952/(2 ** i)) for i in range(7)]
+# Bitboards that represent the horizontal mask
+_HORIZONTAL = [127 * (128 ** i) for i in range(7)]
 class GameState:
 
     # Initialise the game state.
@@ -29,6 +32,7 @@ class GameState:
             'exit':0
             }
         self.last_move = []
+        self.weights = [1,0.2,0.2]
 
     # Function that converts coordinate to bitboard index.
     def coorToBitboard(self,q,r):
@@ -55,45 +59,22 @@ class GameState:
 
     # Function that gets all the pieces on the board
     def getPositions(self, colour):
-        # Bitboards that represent the vertical mask
-        vertical = [int(283691315109952/(2 ** i)) for i in range(7)]
-        # Bitboards that represent the horizontal mask
-        horizontal = [127 * (128 ** i) for i in range(7)]
         player = self.getPlayer(colour)['board']
         result = []
 
         # Check if there is a piece on a column
-        for i in range(len(vertical)):
+        for i in range(len(_VERTICAL)):
             # Skip the column if there's no piece
-            if (vertical[i] & player) == 0:
+            if (_VERTICAL[i] & player) == 0:
                 continue
             # Check the rows if there's a piece on a column
-            for j in range(len(horizontal)):
-                if (horizontal[j] & player & vertical[i]) == 0:
+            for j in range(len(_HORIZONTAL)):
+                if (_HORIZONTAL[j] & player & _VERTICAL[i]) == 0:
                     continue
-                # Add the coordinate of the piece to the result list
+                # Add the coordinate of the piece to the result lists
                 result.append((i -3 ,3 - j))
         # Return the result
         return result
-
-    # Function that determines the score of the board
-    def evaluate(self, colour_i):
-        # Initialise the score
-        score = 0
-        colours = ['red','green','blue']
-        for colour in colours:
-            if colour == colour_i:
-                # Check the number of pieces the player has
-                # piece = len([1 for i in list("{:049b}".format(state.getPlayer(colour)['board'])) if i == "1"])
-                score += self.getPlayer(colour_i)['score']
-                score += self.getPlayer(colour_i)['exit']
-            else:
-                score -= self.getPlayer(colour)['score']
-                score -= self.getPlayer(colour)['exit']
-
-        return score
-
-
 
     # Function that check if there's a piece on a tile.
     def findTile(self,coor):
@@ -128,19 +109,18 @@ class GameState:
             # Check if the piece is on top of a goal.
             if (x,y) in self.getPlayer(colour)['goals']:
                 result.append(("EXIT",(int(x),int(y))))
-                continue
+                break
 
             # Range adapted from game.py.
             ran = range(-3, +3+1)
-            hexes = {(q,r) for q in ran for r in ran if -q-r in ran}
             # All the directions.
             directions = [(-1,0),(0,-1),(1,-1),(1,0),(0,1),(-1,1)]
             for (a,b) in directions:
                 # Check if the tile in the direction is available.
-                if (x+a,y+b) in hexes and not self.findTile((x+a,y+b)):
+                if self.isValidMove((x+a,y+b)) and not self.findTile((x+a,y+b)):
                     result.append(("MOVE",((x,y), (int(x+a),int(y+b)))))
                 # If not, check the next tile in the same direction.
-                elif (x+2*a,y+2*b)in hexes and not self.findTile((x+2*a,y+2*b)):
+                elif self.isValidMove((x+2*a,y+2*b)) and not self.findTile((x+2*a,y+2*b)):
                     result.append(("JUMP",((x,y), (int(x+2*a),int(y+2*b)))))
 
                 # Return PASS if there's no available move.
@@ -148,16 +128,10 @@ class GameState:
                 result.append(("PASS",None))
         return result
 
-    # Check if there is a winner.
-    def winner(self):
-        if self.red['exit'] == 4:
-            return "red"
-        elif self.green['exit'] == 4:
-            return "green"
-        elif self.blue['exit'] == 4:
-            return "blue"
-        else:
-            return None
+    def isValidMove(self, coor):
+        ran = range(-3, 4)
+        return coor[0] in ran and coor[1] in ran and -coor[0]-coor[1] in ran
+
 
     # Function that undo the last move.
     def undo(self):
@@ -209,9 +183,49 @@ class GameState:
             return (tile,midtile)
         return None
 
+    def heuristic(self, colour_i):
+        result = 0
+        i = 0
+        colours = ["red", "green", "blue"]
+
+        for colour in colours:
+            #Modify update to record : No of Opponent/our Jumps, No of Exits
+            # Adds self ave_dist score
+            if colour == colour_i:
+                if len(self.getPositions(colour)) != 0:
+                    result += self.getPlayer(colour)['score']  * 10
+                    result -= (self.weights[i] * self.aveDist(colour))
+                    result += self.getPlayer(colour_i)['exit'] * 100
+            else:
+                # Deducts opponent's ave_dist score
+                if len(self.getPositions(colour)) != 0:
+                    result += (self.weights[i] * self.aveDist(colour))
+                    result -= self.getPlayer(colour_i)['score']
+                    result -= self.getPlayer(colour_i)['exit']
+
+
+        return result
+
+
+
+     #Calculates the average distance of pieces of a colour to
+    #their respective closest goal
+    def aveDist(self, colour):
+        ave_dist = 0
+        #Iterate self pieces and for each piece get min dist then sum all
+        for piece in self.getPositions(colour):
+            min_dist = sys.maxsize
+            for goal in self.getPlayer(colour)['goals']:
+                dist = self.hex_distance(piece,goal)
+                if dist < min_dist:
+                    min_dist = dist
+            ave_dist += min_dist
+        ave_dist = ave_dist/len(self.getPositions(colour))
+        return ave_dist
+
     # Helper function that calculates the distance between two tiles.
     # Adapted from https://www.redblobgames.com/grids/hexagons.
-    def tileDistance(self,a,b):
+    def hex_distance(self,a,b):
         return (abs(a[0] - b[0])
           + abs(a[0] + a[1] - b[0] - b[1])
           + abs(a[1] - b[1])) / 2
@@ -220,15 +234,20 @@ class GameState:
     def updateState(self, colour, action):
         # Store the move.
         self.last_move.append({
-            "red":{"board":self.red['board'],"goals":self.red['goals'],"score":int(self.red['score']),"exit":int(self.red['exit'])},
-            "green":{"board":self.green['board'],"goals":self.green['goals'],"score":int(self.green['score']),"exit":int(self.green['exit'])},
-            "blue":{"board":self.blue['board'],"goals":self.blue['goals'],"score":int(self.blue['score']),"exit":int(self.blue['exit'])},
+            #"red":{"board":self.red['board'],"goals":self.red['goals'],"score":int(self.red['score']),"exit":int(self.red['exit'])},
+            #"green":{"board":self.green['board'],"goals":self.green['goals'],"score":int(self.green['score']),"exit":int(self.green['exit'])},
+            #"blue":{"board":self.blue['board'],"goals":self.blue['goals'],"score":int(self.blue['score']),"exit":int(self.blue['exit'])},
+            "red":deepcopy(self.red),
+            "green":deepcopy(self.green),
+            "blue":deepcopy(self.blue)
         })
+
+        player = self.getPlayer(colour)
 
         # MOVE.
         if action[0] == "MOVE":
-            self.getPlayer(colour)['board'] = self.move(self.getPlayer(colour)['board'], action[1][0],action[1][1])
-            self.getPlayer(colour)['score'] -= 2
+            player['board'] = self.move(player['board'], action[1][0],action[1][1])
+            player['score'] -= 5
         # JUMP.
         if action[0] == "JUMP":
             # Check if there's a piece in between the jumps.
@@ -238,19 +257,20 @@ class GameState:
                 if (check[0] != colour):
                     # Flip the piece.
                     self.getPlayer(check[0])['board'] = self.addrmPiece(self.getPlayer(check[0])['board'],check[1])
-                    self.getPlayer(colour)['board'] = self.addrmPiece(self.getPlayer(colour)['board'],check[1],True)
+                    player['board'] = self.addrmPiece(player['board'],check[1],True)
                     self.getPlayer(check[0])['score'] -= 1
-                    self.getPlayer(colour)['score'] += 1
+                    player['score'] += 1
 
                 # Update the jumped position.
-                self.getPlayer(colour)['board'] = self.move(self.getPlayer(colour)['board'], action[1][0],action[1][1])
+                player['score'] += 1
+                player['board'] = self.move(player['board'], action[1][0],action[1][1])
 
         # EXIT
         if action[0] == "EXIT":
             # Remove the piece from the board and update the player score.
-            self.getPlayer(colour)['board'] = self.addrmPiece(self.getPlayer(colour)['board'],action[1])
-            self.getPlayer(colour)['score'] += 10
-            self.getPlayer(colour)['exit'] += 10
+            player['board'] = self.addrmPiece(player['board'],action[1])
+            player['score'] += 1
+            player['exit'] += 100
 
 class Player:
     def __init__(self, colour):
@@ -295,25 +315,17 @@ class Player:
             result[move] = value
         # Return the move with the best score or return PASS if no move is
         # available.
+        print(result)
         return max(result, key=result.get) if len(result) != 0 else ('PASS',None)
 
     # Minimax algorithm
     def minimax(self, state, colour, depth, maxPlayer, a, b, score = None, exit = None):
-        """
-        # Check if it is a terminal state .
-        if state.winner():
-            # Return max score if the player wins
-            # or return minimum score other players win.
-            if state.winner() == colour:
-                return 50000
-            else:
-                return -50000
-        """
 
         # Evaluate the board if it reaches the maximum depth
-        if depth == 0 or state.winner():
+        #if depth == 0 or state.winner():
+        if depth == 0:
 
-            return state.evaluate(colour)
+            return state.heuristic(colour)
 
         # Check the max
         if maxPlayer:
